63910d08f52566e2ee85a5391e7cf89115716b61
Dirk Engling <erdgeist@erdgeist.org>
2012-09-25 14:34:40 +0200

    Add a pure IP tracker to test

--- main.c
+++ main.c
@@ -16,6 +16,7 @@ int main( int argc, char ** argv ) {
   bep034_lookup( "udp://foo:bar@tracker.ccc.de:90/" );
   bep034_lookup( "[2001::7]" );
   bep034_lookup( "[2001::7]:70/tracker/announce" );
+  bep034_lookup( "127.0.0.1" );
 
   /* Hang around for a while */
   sleep( 100 );
