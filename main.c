368d2f446d16ed303a63e2580df860a61172dd43
Dirk Engling <erdgeist@erdgeist.org>
2012-09-27 01:20:22 +0200

    Add a host with valid BITTORRENT tracker records

--- main.c
+++ main.c
@@ -17,6 +17,7 @@ int main( int argc, char ** argv ) {
   bep034_lookup( "[2001::7]" );
   bep034_lookup( "[2001::7]:70/tracker/announce" );
   bep034_lookup( "127.0.0.1" );
+  bep034_lookup( "tracker.openbittorrent.com" );
 
   /* Hang around for a while */
   sleep( 100 );
